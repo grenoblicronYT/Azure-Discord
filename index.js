950a90dc5b9f88a03cb86be626d75c2a7c6ebe51
leo fiorellini <pro.leofiirellini@gmail.com>
2021-01-13 19:32:38 +0100

    Ajout de pronote

--- index.js
+++ index.js
@@ -2,7 +2,28 @@ const config = require('dotenv').config({path: __dirname + '/.env'})
 const ytdl = require('ytdl-core')
 const discord = require('discord.js')
 const client = new discord.Client()
+const pronote = require('pronote-api')
 const token = process.env.DISCORD_KEY
+
+
+async function Pronote(url, username, password, _msg)
+{
+    const session = await pronote.login(url, username, password/*, cas*/);
+    
+    _msg.author.send(session.user.name); // Affiche le nom de l'élève
+    _msg.author.send(session.user.studentClass.name); // Affiche la classe de l'élève
+    
+    const timetable = await session.timetable(); // Récupérer l'emploi du temps d'aujourd'hui
+    const marks = await session.marks(); // Récupérer les notes du trimestre
+    
+    _msg.author.send(`L'élève a ${timetable.length} cours aujourd'hui`); 
+    _msg.author.send(`et a pour l'instant une moyenne de ${marks.averages.student} ce trimestre.`);
+    
+    // etc. les fonctions utilisables sont 'timetable', 'marks', 'contents', 'evaluations', 'absences', 
+    // 'homeworks', 'infos', et 'menu', sans oublier les champs 'user' et 'params' qui regorgent d'informations.
+}
+
+
 
 client.login(token)
 
@@ -33,6 +54,24 @@ client.on('message', async (message) => {
           }
         
           
+    }
+
+    if (message.content.startsWith('*pronote')) {
+      const msg = message
+      const command = msg.content.split(' ')
+      message.delete()
+      Pronote(process.env.pronote_url, command[1], command[2], msg).catch(err => {
+        if (err.code === pronote.errors.WRONG_CREDENTIALS.code) {
+            msg.channel.send('Mauvais identifiants');    
+        } else {
+            if (err.code === 2) {
+              msg.channel.send(err.message)
+            }
+            console.error(err);
+        }
+    });
+    
+      
     }
 
     
